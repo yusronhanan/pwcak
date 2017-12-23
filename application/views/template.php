404f9e77cc3d978b6893496f517fb0fd7b64b825
Karina Widhia Nugrahini <karinwidhia@gmail.com>
2017-12-23 13:14:18 +0700

    notif dropdown

--- application/views/template.php
+++ application/views/template.php
@@ -34,6 +34,70 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 				if ($this->session->userdata('logged_in') == TRUE) {
 					
 				 ?>
+
+				 <li class="dropdown at-drop">
+		              <a href="#" class="dropdown-toggle dropdown-at " data-toggle="dropdown"><i class="fa fa-globe"></i>NOTIF</a>
+		              <ul class="dropdown-menu menu1 " role="menu">
+		                <li><a href="#">
+		               
+		                	<div class="user-new">
+		                	<p>New user registered</p>
+		                	<span>40 seconds ago</span>
+		                	</div>
+		                	<div class="user-new-left">
+		                
+		                	<i class="fa fa-user-plus"></i>
+		                	</div>
+		                	<div class="clearfix"> </div>
+		                	</a></li>
+		                <li><a href="#">
+		                	<div class="user-new">
+		                	<p>Someone special liked this</p>
+		                	<span>3 minutes ago</span>
+		                	</div>
+		                	<div class="user-new-left">
+		                
+		                	<i class="fa fa-heart"></i>
+		                	</div>
+		                	<div class="clearfix"> </div>
+		                </a></li>
+		                <li><a href="#">
+		                	<div class="user-new">
+		                	<p>John cancelled the event</p>
+		                	<span>4 hours ago</span>
+		                	</div>
+		                	<div class="user-new-left">
+		                
+		                	<i class="fa fa-times"></i>
+		                	</div>
+		                	<div class="clearfix"> </div>
+		                </a></li>
+		                <li><a href="#">
+		                	<div class="user-new">
+		                	<p>The server is status is stable</p>
+		                	<span>yesterday at 08:30am</span>
+		                	</div>
+		                	<div class="user-new-left">
+		                
+		                	<i class="fa fa-info"></i>
+		                	</div>
+		                	<div class="clearfix"> </div>
+		                </a></li>
+		                <li><a href="#">
+		                	<div class="user-new">
+		                	<p>New comments waiting approval</p>
+		                	<span>Last Week</span>
+		                	</div>
+		                	<div class="user-new-left">
+		                
+		                	<i class="fa fa-rss"></i>
+		                	</div>
+		                	<div class="clearfix"> </div>
+		                </a></li>
+		                <li><a href="<?php echo base_url(); ?>notif" class="view">View all messages</a></li>
+		              </ul>
+		            </li>
+
 				<li><a href="<?php echo base_url() ?>auth/logout" ><i class="fa fa-close" aria-hidden="true"></i>LOGOUT</a></li>
 				<?php 
 				}

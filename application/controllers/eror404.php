1c970d2b0f988992d7af829fab29af1dde91fb64
Qoriatul Masfufah <qoriatulmasfufah@gmail.com>
2017-12-28 23:47:09 +0700

    404_page_not_found

---
+++ application/controllers/eror404.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Eror404 extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('404_view');
+
+	}
+
+}
+
+/* End of file eror404.php */
+/* Location: ./application/controllers/eror404.php */
---
+++ application/views/404_view.php
@@ -0,0 +1,126 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>404 Page Not Found</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Minimal Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<!-- Custom Theme files -->
+<link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css" media="all">
+<link href="<?php echo base_url(); ?>assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all"> 
+<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"> </script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"> </script>
+</head>
+
+<style>
+html, body{
+  	font-family:'arial';
+    font-size: 100%;
+  	background:#F3F3F4;
+  	overflow-x: hidden;
+}
+	/*--404--*/
+.four{
+	text-align: center;
+	    padding: 3em 0;
+}
+.four a{
+	font-size: 0.9em;
+    padding: 0.5em 1em;
+    color: #fff;
+    display: block;
+    text-decoration: none;
+    background: #d95459;
+    margin: 0 auto;
+    width: 10%;
+}
+.four p{
+	font-size: 1em;
+    padding: 0.5em 0em 2em;
+    display: block;
+    font-family: 'arial';
+
+}
+/* Shutter In Horizontal */
+.hvr-shutter-in-horizontal {
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: translateZ(0);
+  transform: translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  -moz-osx-font-smoothing: grayscale;
+  position: relative;
+  background: #d95459;
+  -webkit-transition-property: color;
+  transition-property: color;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+   width: 100%;
+}
+.hvr-shutter-in-horizontal:before {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #1abc9c;
+  -webkit-transform: scaleX(1);
+  transform: scaleX(1);
+  -webkit-transform-origin: 50%;
+  transform-origin: 50%;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -webkit-transition-timing-function: ease-out;
+  transition-timing-function: ease-out;
+}
+.hvr-shutter-in-horizontal:hover, .hvr-shutter-in-horizontal:focus, .hvr-shutter-in-horizontal:active {
+  color: white;
+}
+.hvr-shutter-in-horizontal:hover:before, .hvr-shutter-in-horizontal:focus:before, .hvr-shutter-in-horizontal:active:before {
+  -webkit-transform: scaleX(0);
+  transform: scaleX(0);
+}
+.login-sub{
+	background: #1abc9c;
+}
+.login-sub:before {
+	background: #d95459; 
+}
+</style>
+
+<body>
+	<div class="four">
+		<img src="<?php echo base_url(); ?>assets/images/404.png" alt="" />
+		<p>For Some Reason The Page You Requested Could Not Be Found On Our Server</p>
+		<a href="#" class="hvr-shutter-in-horizontal">Go To Home</a>
+	</div>
+		<!---->
+	<!-- <div class="copy-right">
+            <p> &copy; 2016 Minimal. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>	    
+    </div> -->
+		
+     
+<!---->
+<!--scrolling js-->
+	<script src="<?php echo base_url(); ?>assets/js/jquery.nicescroll.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
+	<!--//scrolling js-->
+</body>
+</html>
+
